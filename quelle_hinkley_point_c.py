9f98fd5e6362b33f3286ca8dffba66770c58fd7d
kaipotthoff <kppotthoff@web.de>
2025-01-13 15:49:50 +0100

    Add files via upload

---
+++ quelle_hinkley_point_c.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("Quellenübersicht für die Lernkarte Hinkley Point C")
+
+st.markdown("""
+### Quellen:
+1. [Quelle 1 - Auszug Hinkley Point C - CFD-Register UK](https://register.lowcarboncontracts.uk/NUC-HPC-198/)
+2. [Quelle 2 - Kostenschätzung Hinkley Point C 2015](https://www.gov.uk/government/publications/decc-government-major-projects-portfolio-data-2015)
+3. [Quelle 3 - Kostenschätzung Hinkley Point C 2024](https://www.bbc.com/news/business-68073279)
+4. [Quelle 4 - Verschuldung EDF 2024](https://montelnews.com/de/news/5e3b48c1-0d32-4b6e-8711-e00c233409a9/geldfrage-konnte-frankreichs-atom-plane-bremsen-experten#:~:text=EDF%20w%C3%BCrde%20Investoren%20finden%2C%20da,von%20mehr%20als%20100%20Mrd.)
+5. [Quelle 5 - BIP Frankreich 2023](https://www.destatis.de/Europa/DE/Staat/EU-Staaten/Frankreich.html)
+6. [Quelle 6 - Abschätzung CFD Vertrag Inflationsentwicklung 2027](https://www.iwrpressedienst.de/energie-themen/pm-7986-hinkley-point-c-strom-aus-neuem-britischem-atomkraftwerk-kostet-ueber-15-cent-pro-kilowattstunde)
+7. [Quelle 7 - Weitere Subventionen für Hinkley Point C](https://ec.europa.eu/competition/state_aid/cases/251157/251157_1615983_2292_4.pdf)
+8. [Quelle 8 - Abschätzung LCOE Hinkley Point C und neue AKW auf Basis von Lazard US-Studie](https://illuminem.com/illuminemvoices/nuclear-economics-lessons-from-lazard-to-hinkley-point-c)
+""")
+
